0feff7ecaccab743b9b7b40f8373a642a24d464d
Jackson Tam <Jackson.Tam@OOCL.com>
2024-12-10 11:24:19 +0800

    [3] feat; connect backend with frontend

--- src/App.js
+++ src/App.js
@@ -2,29 +2,14 @@ import React from "react";
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Home from "./components/Home";
-import About from "./components/About/About";
 import ConcertEvent from "./components/ConcertEvent";
 import UserCharityWrapper from "./components/Charity/UserCharityWrapper";
-import { Container, AppBar, Toolbar, Typography, Button, Box } from "@mui/material";
 import NavigationBar from './components/NavigationBar';
 import Events from './components/Events';
 import UserProfile from './components/UserProfile';
 import Ticket from './components/Ticket';
 
 function App() {
-  const event = {
-    id: 1,
-    poster:
-        "https://www.allegiantstadium.com/assets/img/Static_Venue_800x500_Coldplay_2025_NA_Regional_AllegiantStadium_0606-05e3ffdf12.jpg",
-    title: "Live Concert: The Rockers",
-    time: "7:00 PM, Dec 15, 2024",
-    venue: "Stadium Arena, NY",
-    priceRange: "$50 - $200",
-  };
-
-  const handleBuyTicket = () => {
-    alert("Redirecting to ticket purchase...");
-  };
 
   return (
     <body>
@@ -38,11 +23,11 @@ function App() {
             <Route path="/concert-event" element={<ConcertEvent />} />
               <Route path="/userCharityWrapper" element={<UserCharityWrapper />} />
             <Route path="/user-profile" element={<UserProfile />} />
-            <Route path="*" element={<h1>Not Found</h1>} />          
+            <Route path="*" element={<h1>Not Found</h1>} />
             <Route path="/userCharityWrapper" element={<UserCharityWrapper width="1000px" />} />
             <Route
                 path="/concert-event"
-                element={<ConcertEvent event={event} onBuyTicket={handleBuyTicket} />}
+                element={<ConcertEvent />}
             />
         </Routes>
         </Router>
--- src/api/interceptor.js
+++ src/api/interceptor.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseURL = 'http://localhost:8080';
+const baseURL = 'http://localhost:7002';
 
 const instance = axios.create({baseURL});
 
