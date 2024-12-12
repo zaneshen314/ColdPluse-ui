f2d4dc7f9a543de5f2e0bba8af6865ca079978bf
SHENZA <shenza@oocl.com>
2024-12-12 11:16:00 +0800

    fix,fix 401

--- src/api/interceptor.js
+++ src/api/interceptor.js
@@ -1,15 +1,20 @@
 import axios from "axios";
+import {message} from "antd";
+import {useNavigate} from "react-router-dom";
 
 const baseURL = process.env.REACT_APP_BASE_URL || 'http://localhost:7002';
 
 const instance = axios.create({baseURL});
-
+const navigate = useNavigate();
 instance.interceptors.response.use(
     (response) => {
         return response;
     },
     (error)=>{
         console.log("Error: ", error);
+        if (error.response && error.response.status === 401) {
+            message.warning('Session Expired. Please log in again.').then(r => navigate('/login'));
+        }
         return Promise.reject(error);
 }
 );
