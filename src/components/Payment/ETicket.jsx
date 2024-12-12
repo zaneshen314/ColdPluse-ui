75c952bc9fd1969b922d748e829006076f46159c
Emily Ng <ngem3@oocl.com>
2024-12-12 21:32:31 +0800

    [6]chore: style eticket component

--- src/components/Payment/ETicket.jsx
+++ src/components/Payment/ETicket.jsx
@@ -4,11 +4,12 @@ import QRCode from 'qrcode.react';
 
 const ETicket = ( { ticket, large } ) => {
   const { id, idCardNum, viewerName } = ticket;
+  const typographyStyle = { mb: 1, fontWeight: 'bold' };
 
   return (
-    <Box my={1} border={1} borderColor="grey.500" p={2}>
-      <Typography variant={large ? "h5" : "h6"}>Guest ID: {idCardNum}</Typography>
-      <Typography variant={large ? "h5" : "h6"}>Guest Name: {viewerName}</Typography>
+    <Box my={1} border={1} borderColor="grey.500" p={2} sx={{backgroundColor: 'rgba(29, 29, 29, 0.3)', borderRadius:'10px', margin:'2rem'}}>
+      <Typography variant={"h6"} sx={typographyStyle}>Guest ID: {idCardNum}</Typography>
+      <Typography variant={"h6"} sx={typographyStyle}>Guest Name: {viewerName}</Typography>
       <Box mt={2} />
       <QRCode value={id} size={large ? 192 : 128} />
     </Box>
--- src/components/Payment/ETicketGroup.jsx
+++ src/components/Payment/ETicketGroup.jsx
@@ -1,39 +1,69 @@
 import React from 'react';
-import { Container, Typography, Box, Grid } from '@mui/material';
+import { Container, Typography, Box, Grid, CardContent, Card } from '@mui/material';
 import ETicket from './ETicket';
 import Carousel from '../Charity/Carousel';
-
+import AccessTimeIcon from '@mui/icons-material/AccessTime';
+import LocationOnIcon from '@mui/icons-material/LocationOn';
+import ConfirmationNumberIcon from '@mui/icons-material/ConfirmationNumber';
+import CalendarTodayIcon from '@mui/icons-material/CalendarToday';
+import { Divider } from 'antd';
 const ETicketGroup = ( { tickets, concertDetails, purchasedTime } ) => {
+  // borderColor: 'rgba(228, 219, 233, 0.25)'
+  const typographyStyle = { mb: 1, display: 'flex', alignItems: 'center' };
+  const iconStyle = { verticalAlign: 'middle', mr: 1 };
 
   return (
     <Container maxWidth="md" style={{ marginTop: '10rem' }}>
-      <Typography variant="h3" gutterBottom  align="left">
-        Purchase Success
-      </Typography>
-      <Grid container spacing={3}>
-      <Grid item xs={12} md={6} style={{ textAlign: 'center' }}>
-        <Typography variant="h4" gutterBottom align="left">
-          Purchased Time: {purchasedTime}
-        </Typography>
-        <Box mt={3}>
-          <Typography variant="h4" align="left">Concert Details:</Typography>
-          <Typography variant="h5" align="left">Concert Name: {concertDetails.name}</Typography>
-          <Typography variant="h5" align="left">Time: {concertDetails.time}</Typography>
-          <Typography variant="h5" align="left">Venue: {concertDetails.venue}</Typography>
-          <Typography variant="h5" align="left">Class: {concertDetails.ticketClass}</Typography>
-        </Box>
-      </Grid>
-        <Grid item xs={12} md={6} style={{ textAlign: 'center' }}>
-          <Typography variant="h4">Ticket(s):</Typography>
-          {
-            tickets.length > 1 ?(
-              <Carousel slidesToShow={1} children={tickets.map((ticket, index)=> <ETicket key={index} ticket={ticket} large />)}/>
-            ):(
-              <ETicket ticket={tickets[0]} large />
-           )
-          }   
-        </Grid>
-      </Grid>
+      <Card elevation={3} sx={{ backgroundColor: 'rgba(29, 29, 29, 0.3)', color: 'white' }}>
+        <CardContent>
+          <Typography variant="h3" gutterBottom  align="center">
+            Transaction Successful
+          </Typography>
+          <Typography variant="body1" align="center">
+              <AccessTimeIcon sx={iconStyle} />
+              Purchased Time: {purchasedTime}
+          </Typography>
+          <Divider style={{ margin: '1rem 0', backgroundColor:'rgba(228, 219, 233, 0.25)'}} />
+          <Grid container spacing={3}>
+          <Grid item xs={12} md={6} style={{ textAlign: 'center' }}>
+            <Box mt={3}>
+              <Typography variant="h5" sx={{fontWeight: 'bold' }}>
+                Concert: {concertDetails.name}
+              </Typography>
+              <Box sx={{ my: 2 }}>
+                  <Typography variant="body1" sx={typographyStyle}>
+                      <CalendarTodayIcon sx={iconStyle} />
+                      Time: {new Date(concertDetails.time).toLocaleString()}
+                  </Typography>
+                  <Typography variant="body1" sx={typographyStyle}>
+                      <LocationOnIcon sx={iconStyle} />
+                      Venue: {concertDetails.venue}
+                  </Typography>
+                  <Typography variant="body1" sx={typographyStyle}>
+                      <ConfirmationNumberIcon sx={iconStyle} />
+                      Class: {concertDetails.ticketClass}
+                  </Typography>                  
+              </Box>
+            </Box>
+          </Grid>
+            <Grid item xs={12} md={6} style={{ textAlign: 'center' }}>
+              <Box sx={{ my: 2 }}>
+                <Typography variant="h5" sx={{fontWeight: 'bold' }}>
+                  Ticket{tickets.length > 1 ? 's' : ''} Purchased
+                </Typography>
+                {
+                  tickets.length > 1 ?(
+                    <Carousel slidesToShow={1} children={tickets.map((ticket, index)=> 
+                    <ETicket key={index} ticket={ticket} large />)}/>
+                  ):(
+                    <ETicket ticket={tickets[0]} large />
+                  )
+                }   
+              </Box>
+            </Grid>
+          </Grid>
+        </CardContent>
+      </Card>
     </Container>
   );
 };
