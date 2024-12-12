02b8bf9929fd43b72c056454bb775c6832355086
angus-chanan11 <angus.chan@oocl.com>
2024-12-12 11:28:31 +0800

    fix: improve E-ticket display style

--- src/components/Payment/ETicket.jsx
+++ src/components/Payment/ETicket.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 import { Typography, Box } from '@mui/material';
 import QRCode from 'qrcode.react';
 
-const ETicket = ( { ticket } ) => {
+const ETicket = ( { ticket, size } ) => {
   const { id, idCardNum, viewerName } = ticket;
 
   return (
     <Box my={1} border={1} borderColor="grey.500" p={2}>
       <Typography variant="h6">Guest ID: {idCardNum}</Typography>
       <Typography variant="h6">Guest Name: {viewerName}</Typography>
-      <QRCode value={id} size={128} />
+      <QRCode value={id} size={size ?? 128} />
     </Box>
   );
 };
--- src/components/Payment/ETicketGroup.jsx
+++ src/components/Payment/ETicketGroup.jsx
@@ -4,24 +4,24 @@ import ETicket from './ETicket';
 
 const ETicketGroup = ( { tickets, concertDetails, purchasedTime } ) => {
   return (
-    <Container maxWidth="sm" style={{ textAlign: 'center', marginTop: '50px' }}>
+    <Container maxWidth="sm" style={{ textAlign: 'center', marginTop: '3rem' }}>
       <Typography variant="h4" gutterBottom>
         Purchase Success
       </Typography>
       <Typography variant="h5" gutterBottom>
         Purchased Time: {purchasedTime}
       </Typography>
-      <Box mt={4}>
+      <Box mt={3}>
         <Typography variant="h5">Concert Details:</Typography>
         <Typography variant="body1">Concert Name: {concertDetails.name}</Typography>
         <Typography variant="body1">Time: {concertDetails.time}</Typography>
         <Typography variant="body1">Venue: {concertDetails.venue}</Typography>
         <Typography variant="body1">Class: {concertDetails.ticketClass}</Typography>
       </Box>
-      <Box>
+      <Box mt={3}>
         <Typography variant="h5">Ticket(s):</Typography>
         {tickets.map((ticket, index) => 
-          <ETicket key={index} ticket={ticket}/>
+          <ETicket key={index} ticket={ticket} size={192}/>
         )}
       </Box>
     </Container>
